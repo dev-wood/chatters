bb33aa49797193aa709876246e5f047adae99337
devwoodo <dev.woodok@gmail.com>
2016-04-08 17:48:16 +0900

    N Implement: *List class, PacketStream_Base class

--- chatters/header_common.cpp
+++ chatters/header_common.cpp
@@ -1,24 +1,98 @@
 #include "header_common.h"
 
 
+UserKey UserInfoToken::getUniqueKey()
+{
+	return UniqueKey++;
+}
+
 UserInfoToken::UserInfoToken()
 {
-	key = UniqueKey++;
+	key = getUniqueKey();
+}
+UserInfoToken::UserInfoToken(UserInfoToken&& tk)
+{
+	*this = std::move(tk);
+}
+UserInfoToken & UserInfoToken::operator=(UserInfoToken&& tk)
+{
+	if (this == &tk) 
+		return *this;
+
+	this->key = tk.key;
+	std::swap(this->id, tk.id);
+
+	return *this;
 }
 UserInfoToken::UserInfoToken(const std::string& _id)
 {
 	id = _id;
-	key = UniqueKey++;
+	key = getUniqueKey();
+}
+
+UserKey RoomInfoToken::getUniqueKey()
+{
+	return UniqueKey++;
 }
 
 RoomInfoToken::RoomInfoToken()
 {
 	numOfPeer = 0;
-	key = UniqueKey++;
+	key = getUniqueKey();
+}
+RoomInfoToken::RoomInfoToken(RoomInfoToken && tk)
+{
+	*this = std::move(tk);
+}
+RoomInfoToken & RoomInfoToken::operator=(RoomInfoToken && tk)
+{
+	if (this == &tk)
+		return *this;
+
+	this->key = tk.key;
+	std::swap(this->title, tk.title);
+	
+	return *this;
 }
 RoomInfoToken::RoomInfoToken(const std::string& _title)
 {
 	title = _title;
 	numOfPeer = 0;
-	key = UniqueKey++;
+	key = getUniqueKey();
+}
+
+void UserInfoTokenList::add(UserInfoToken tk)
+{
+	list.insert(std::make_pair(tk.key, tk));
+}
+
+bool UserInfoTokenList::remove(UserKey uKey)
+{
+	if (list.erase(uKey))
+		return true;
+	else 
+		return false;
+}
+
+UserInfoToken & UserInfoTokenList::find(UserKey uKey)
+{
+	return (list.find(uKey))->second;
+}
+
+void RoomInfoTokenList::add(RoomInfoToken tk)
+{
+	list.insert(std::make_pair(tk.key, tk));
+}
+
+bool RoomInfoTokenList::remove(RoomKey rKey)
+{
+	if (list.erase(rKey))
+		return true;
+	else
+		return false;
+}
+
+RoomInfoToken & RoomInfoTokenList::find(RoomKey rKey)
+{
+	return (list.find(rKey))->second;
 }
--- chatters/header_common.h
+++ chatters/header_common.h
@@ -7,7 +7,7 @@
 #include <string>
 #include <sstream>
 #include <queue>
-#include <map>
+#include <unordered_map>
 
 typedef int UserKey;
 typedef int RoomKey;
@@ -15,12 +15,16 @@ typedef int RoomKey;
 class UserInfoToken
 {
 private:
-	static int UniqueKey;
+	static UserKey UniqueKey;
 public:
 	UserKey key;
 	std::string id;
+private:
+	UserKey getUniqueKey();
 public:
 	UserInfoToken();
+	UserInfoToken(UserInfoToken&&);
+	UserInfoToken& operator=(UserInfoToken&&);
 	UserInfoToken(const std::string& _id);
 };
 int UserInfoToken::UniqueKey = 0;
@@ -28,18 +32,89 @@ int UserInfoToken::UniqueKey = 0;
 class RoomInfoToken
 {
 private:
-	static int UniqueKey;
+	static RoomKey UniqueKey;
 public:	
 	RoomKey key;
 	std::string title;
 	int numOfPeer;
+private:
+	UserKey getUniqueKey();
 public:
 	RoomInfoToken();
+	RoomInfoToken(RoomInfoToken&&);
+	RoomInfoToken& operator=(RoomInfoToken&&);
 	RoomInfoToken(const std::string& _title);
 
 };
 int RoomInfoToken::UniqueKey = 0;
 
+class UserInfoTokenList
+{
+private:
+	std::unordered_map<UserKey, UserInfoToken> list;
+public:
+	void add(UserInfoToken tk);
+	bool remove(UserKey uKey);
+	UserInfoToken& find(UserKey uKey);
 
+public:
+
+};
+
+class RoomInfoTokenList
+{
+private:
+	std::unordered_map<RoomKey, RoomInfoToken> list;
+public:
+	void add(RoomInfoToken tk);
+	bool remove(RoomKey rKey);
+	RoomInfoToken& find(RoomKey rKey);
+
+public:
+
+};
+
+class PacketStream_Base
+{
+public:
+	enum struct PType : int
+	{
+		// Client to Server packet
+		CTOS_LOGIN_REQUEST = 0,
+		
+		CTOS_LOBBY_JOINROOM,
+		CTOS_LOBBY_LOADLIST,
+		CTOS_LOBBY_CREATEROOM,
+		
+		CTOS_CREATEROOM_CREATEROOM,
+		
+		CTOS_CHAT_QUITROOM,
+		CTOS_CHAT_MYSTATEREADY,
+		CTOS_CHAT_MYSTATENOTREADY,
+		CTOS_CHAT_ROOMSTATEPLAYING,
+		CTOS_CHAT_ROOMSTATENOTPLAYING,
+		CTOS_CHAT_CHAT,
+
+		// Server to Client packet
+		STOC_LOGIN_ACCEPT,
+		STOC_LOGIN_FAIL,
+		
+		STOC_LOBBY_CREATEROOMOK,
+		STOC_LOBBY_CREATEROOMFAIL,
+		STOC_LOBBY_JOINROOMOK,
+		STOC_LOBBY_JOINROOMFAIL,
+		STOC_LOBBY_LOADLIST,
+		
+		STOC_CREATEROOM_OK,
+		STOC_CREATEROOM_FAIL,
+		
+		STOC_CHAT_QUITUSER,
+		STOC_CHAT_GUESTSTATEREADY,
+		STOC_CHAT_GUESTSTATENOTREADY,
+		STOC_CHAT_ROOMSTATEPLAYING,
+		STOC_CHAT_ROOMSTATENOTPLAYING,
+		STOC_CHAT_CHAT
+	};
+} PSB;
 
 #endif
