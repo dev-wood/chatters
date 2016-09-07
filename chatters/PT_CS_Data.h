ccc66acee0f9360d1c9477ae40dabdb18fa7a4e6
devwoodo <dev.woodok@gmail.com>
2016-09-07 16:20:13 +0900

    N Merging conflict resolved.

--- chatters/PT_CS_Data.h
+++ chatters/PT_CS_Data.h
@@ -13,11 +13,7 @@ struct PK_CS_LOGIN_REQUEST : public Packet_Base
 public:
 	/* Member method */
 	PK_CS_LOGIN_REQUEST();
-<<<<<<< HEAD
-	PK_CS_LOGIN_REQUEST(PTYPE ptype, const char* buf);
-=======
 	PK_CS_LOGIN_REQUEST(PTYPE ptype, const char* buf, size_t bufLen);
->>>>>>> svtest
 
 	virtual std::shared_ptr<Packet_Base> processPacket(MachObject& targetMObject);	// Received packet process procedure using strategy pattern
 public:
@@ -37,11 +33,7 @@ struct PK_CS_LOBBY_JOINROOM : public Packet_Base
 public:
 	/* Member method */
 	PK_CS_LOBBY_JOINROOM();
-<<<<<<< HEAD
-	PK_CS_LOBBY_JOINROOM(PTYPE ptype, const char* buf);
-=======
 	PK_CS_LOBBY_JOINROOM(PTYPE ptype, const char* buf, size_t bufLen);
->>>>>>> svtest
 
 	virtual std::shared_ptr<Packet_Base> processPacket(MachObject& targetMObject);	// Received packet process procedure using strategy pattern
 public:
@@ -60,11 +52,7 @@ struct PK_CS_LOBBY_LOAD_ROOMLIST : public Packet_Base
 public:
 	/* Member method */
 	PK_CS_LOBBY_LOAD_ROOMLIST();
-<<<<<<< HEAD
-	PK_CS_LOBBY_LOAD_ROOMLIST(PTYPE ptype, const char* buf);
-=======
 	PK_CS_LOBBY_LOAD_ROOMLIST(PTYPE ptype, const char* buf, size_t bufLen);
->>>>>>> svtest
 
 	virtual std::shared_ptr<Packet_Base> processPacket(MachObject& targetMObject);	// Received packet process procedure using strategy pattern
 public:
@@ -82,11 +70,7 @@ struct PK_CS_CREATEROOM_CREATEROOM : public Packet_Base
 public:
 	/* Member method */
 	PK_CS_CREATEROOM_CREATEROOM();
-<<<<<<< HEAD
-	PK_CS_CREATEROOM_CREATEROOM(PTYPE ptype, const char* buf);
-=======
 	PK_CS_CREATEROOM_CREATEROOM(PTYPE ptype, const char* buf, size_t bufLen);
->>>>>>> svtest
 
 	virtual std::shared_ptr<Packet_Base> processPacket(MachObject& targetMObject);	// Received packet process procedure using strategy pattern
 public:
@@ -105,11 +89,7 @@ struct PK_CS_CHAT_QUITROOM : public Packet_Base
 public:
 	/* Member method */
 	PK_CS_CHAT_QUITROOM();
-<<<<<<< HEAD
-	PK_CS_CHAT_QUITROOM(PTYPE ptype, const char* buf);
-=======
 	PK_CS_CHAT_QUITROOM(PTYPE ptype, const char* buf, size_t bufLen);
->>>>>>> svtest
 
 	virtual std::shared_ptr<Packet_Base> processPacket(MachObject& targetMObject);	// Received packet process procedure using strategy pattern
 public:
@@ -128,11 +108,7 @@ struct PK_CS_CHAT_CHAT : public Packet_Base
 public:
 	/* Member method */
 	PK_CS_CHAT_CHAT();
-<<<<<<< HEAD
-	PK_CS_CHAT_CHAT(PTYPE ptype, const char* buf);
-=======
 	PK_CS_CHAT_CHAT(PTYPE ptype, const char* buf, size_t bufLen);
->>>>>>> svtest
 
 	virtual std::shared_ptr<Packet_Base> processPacket(MachObject& targetMObject);	// Received packet process procedure using strategy pattern
 public:
@@ -153,11 +129,7 @@ protected:
 * etc. functions
 -
 *********************************************************************/
-<<<<<<< HEAD
-std::shared_ptr<Packet_Base> extractPacketFromBuffer(char * buf);
-=======
 std::shared_ptr<Packet_Base> extractPacketFromBuffer(char * buf, size_t bufLen);
->>>>>>> svtest
 
 
 
