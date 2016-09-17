edf2dc1d269276f078619721a7910fce16f59207
devwoodo <dev.woodok@gmail.com>
2016-09-17 18:15:49 +0900

    N PK_CS_CHAT_QUITROOM packet implement completed.

--- chatters/PT_CS_Data.cpp
+++ chatters/PT_CS_Data.cpp
@@ -22,7 +22,7 @@ std::shared_ptr<Packet_Base> PK_CS_LOGIN_REQUEST::processPacket(MachObject & tar
 
 	if (agent.db_signin(userId, userPassword))
 	{	// sign in success
-		// add user on user list	//rev
+		// add user on user list
 		if (agent.addUser(userId, sockList[0]) != InfoToken::INVALID_KEY)
 		{
 			// build return packet
@@ -244,34 +244,70 @@ PK_CS_CHAT_QUITROOM::PK_CS_CHAT_QUITROOM(PTYPE ptype, const char * buf, size_t b
 }
 std::shared_ptr<Packet_Base> PK_CS_CHAT_QUITROOM::processPacket(MachObject & targetMObject)
 {
-	return std::shared_ptr<Packet_Base>();
-	/*
 	// casting agent
 	auto& agent = dynamic_cast<SvMach &>(targetMObject);
 
 	// packet processing procedure
+	const auto & rm = agent.findRoom(roomKey)->second;
+	
+	if (agent.leaveRoom(roomKey, userKey))
+	{	// quit room success
+		// build return packet
+		auto rtnShPk = std::make_shared<PK_SC_CHAT_QUITUSER>(nullptr);
 
-	// build return packet
-	auto rtnShPk = std::make_shared<..>(..);
+		// set processing result
+		rtnShPk->setProcessInfo(PkInfo::ProcCode::SUCCESS);
 
-	// set processing result
-	rtnShPk->setProcessInfo(..);
+		// register packet receiver
+		rtnShPk->sockList.reserve(rm.userList.size());
+		rtnShPk->sockList.insert(rtnShPk->sockList.begin(), rm.userList.begin(), rm.userList.end());
 
-	// register packet receiver
-	rtnShPk->sockList.push_back(..);
+		return rtnShPk;
+	}
+	else
+	{	// quit room failed
 
-	return rtnShPk;
-	*/
+		/*
+		// critical error occured. Connection with user reset.
+		agent.removeUser(userKey);
+		
+		// build return packet
+		auto rtnShPk = std::make_shared<PK_SC_CHAT_QUITUSER>(nullptr);
+
+		// set processing result
+		rtnShPk->setProcessInfo(PkInfo::ProcCode::SUCCESS);
+
+		// register packet receiver
+		rtnShPk->sockList.reserve(rm.userList.size());
+		rtnShPk->sockList.insert(rtnShPk->sockList.begin(), rm.userList.begin(), rm.userList.end());
+
+		return rtnShPk;
+		*/
+
+		//rev critical error occured. 이런 경우 처리?
+		//		
+		return nullptr;
+	}	
 }
 void PK_CS_CHAT_QUITROOM::_doSerialProc()
 {
 	// serialize member field depending on the packet type
-
+	_buf << userKey << '|';
+	_buf << roomKey << '|';
 }
 void PK_CS_CHAT_QUITROOM::_doDeserialProc()
 {
 	// deserialize member field depending on the packet type
+	
+	std::string token;
+	
+	// extract user key
+	std::getline(_buf, token, '|');
+	userKey = std::stoi(token);
 
+	// extract room key
+	std::getline(_buf, token, '|');
+	roomKey = std::stoi(token);
 }
 
 /* PK_CS_CHAT_CHAT class */
